df6a22e911aa2af6d8bbcc5385fc15431183dbc1
AlexFernandes00 <a89234@alunos.uminho.pt>
2022-05-04 15:28:02 +0100

    Update utilizador.controller.js

--- backend/controllers/utilizador.controller.js
+++ backend/controllers/utilizador.controller.js
@@ -6,9 +6,17 @@ const connect = require ('../config/connectMySQL');
 function registo (req, callback){
     console.log(req.body);
     const nome = req.body.nome;
+    const apelido = req.body.apelido;
+    const email = req.body.email;
+    const numeroTrabalhador = req.body.numeroTrabalhador;
+    const nif = req.body.nif;
+    const telefone =req.body.telefone;
+    const dataNascimento = req.body.dataNascimento;
+    const password = req.body.password;
 
-    const post = { nome: nome};
-    const query = connect.con.query('INSERT INTO utilizadores SET ?', post, function(err, rows, fields) {
+
+    const post = { nome: nome, apelido: apelido, email: email, numeroTrabalhador: numeroTrabalhador, nif: nif, telefone: telefone, dataNascimento: dataNascimento, password: password};
+    const query = connect.con.query('INSERT INTO utilizador SET ?', post, function(err, rows, fields) {
     console.log(query.sql);
     });
         callback({
