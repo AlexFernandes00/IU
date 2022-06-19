8487d0eabea61afd648fe7e614ce52fce56bf011
anamargarida19 <a89190@alunos.uminho.pt>
2022-06-19 16:29:34 +0100

    ghnjm

--- frontend/user2/fetch/popup.js
+++ frontend/user2/fetch/popup.js
@@ -1,15 +1,21 @@
 window.onload = function () {
     const popup = document.getElementById("popupparque");
-    
-    popup.addEventListener('submit', async function (event) {
+
+    popup.addEventListener('click', async function (event) {
         event.preventDefault();
+        var date = new Date();
+        var hora = date.getHours();       
+        var min = date.getMinutes();
+        var datahora = hora + ':' + min;
             let data = {
-                nomeParque: document.getElementById("parque").value,
-                lotacao: document.getElementById("lotacao").value,
-                lixo: document.getElementById("lixo").value,
-                tempo: document.getElementById("tempo").innerHTML,
+                lotacao: document.querySelector('input[name="radioL"]:checked').value,
+                quantidadeLixo: document.querySelector('input[name="radioLixo"]:checked').value,
+                data: datahora.value,
+                tempo: document.getElementById('input[name="radioTempo"]:checked').innerHTML,    
+                idParque: document.querySelector('#select1').value,
             }
-            fetch(`http://127.0.0.1:8080/criarParque`, {
+            console.log(data)
+            fetch(`http://127.0.0.1:8080/criarInformacao`, {
                 headers: {
                     "Content-Type": "application/json"
                 },
