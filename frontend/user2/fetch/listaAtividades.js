7acf857d0926f2ee20896b094d80c555aec8a922
AlexFernandes00 <a89234@alunos.uminho.pt>
2022-06-21 00:20:38 +0100

    update

--- frontend/user2/fetch/listaAtividades.js
+++ frontend/user2/fetch/listaAtividades.js
@@ -1,27 +1,18 @@
 
 window.onload = () => {
-    logout()
+    //logout()
     
-    const loader = document.querySelector("#loading");
+    //const loader = document.querySelector("#loading");
     
-    loader.classList.add("display");
+    //loader.classList.add("display");
     
       const renderProducts = async () => {
         var myHeaders = new Headers();
-        const tblProdutos = document.getElementById("atividades");
+        const tblProdutos = document.getElementById("containerAtividades");
     
         let strHtml = `
         
-                 <thead> 
-                 <th> Nome </th>
-                 <th> Descrição </th>
-                 <th> Data Início </th>
-                 <th> Data Fim </th>
-                 <th> Parque </th>
-    
-               </tr>
-             </thead>
-             <tbody>
+                 
             `
     
         myHeaders.append("Content-Type", "application/json");
@@ -40,30 +31,43 @@ window.onload = () => {
     
         let atividades = await response.json();
         atividades = atividades.body;
-        console.log(atividades)
+        
         for (let i = 0; i < atividades.length; i++) {
-    
+          
           let nome = atividades[i].nome;
           let descricao = atividades[i].descricao;
-          let dataInicio = atividades[i].dataInicio;
-          let dataFim = atividades[i].dataFim;
+          let dataInicioDia = atividades[i].dataInicio.slice(8, 10);
+          let dataInicioMes = atividades[i].dataInicio.slice(5, 7);
+          let dataFimDia = atividades[i].dataFim.slice(8, 10);
+          let dataFimMes = atividades[i].dataFim.slice(5, 7);
           let nomeParque = atividades[i].nomeParque;
-  
+          let meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
     
           strHtml += `
-                <tr>
-                
-                <td> ${nome} </td>
-                <td> ${descricao} </td>
-                <td> ${dataInicio} </td>
-                <td> ${dataFim} </td>
-                <td> ${nomeParque}</td> 
-              </tr>`
-    
+          <div class="event">
+          <div class="event-left">
+            <div class="event-date">
+              <div class="date">${parseInt(dataInicioDia)+1}</div>
+              <div class="month">${meses[dataInicioMes-1]}</div>
+            </div>
+          </div>
+  
+          <div class="event-right">
+            <h3 class="event-title">${nome}</h3>
+  
+            <div class="event-description">
+            ${descricao}
+            </div>
+  
+            <div class="event-timing">
+              <img src="img/time.png" alt="" /> Termina dia: ${parseInt(dataFimDia)+1}/${parseInt(dataFimMes)}
+            </div>
+          </div>
+        </div>`
         }
         strHtml += "</tbody>"
     
-        loader.style.display="none"
+        //loader.style.display="none"
         tblProdutos.innerHTML = strHtml
     
         // Get the modal
@@ -89,7 +93,11 @@ window.onload = () => {
         }
     
       } 
-      renderProducts()
+     
     
-    
+     renderProducts()
     }
+        
+      
+      
+      
