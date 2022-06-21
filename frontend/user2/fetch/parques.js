c024468b86d5fa2d5d6f18c26a2d3ddcb5e2a5c4
AlexFernandes00 <a89234@alunos.uminho.pt>
2022-06-21 15:16:29 +0100

    update

--- frontend/user2/fetch/parques.js
+++ frontend/user2/fetch/parques.js
@@ -9,9 +9,9 @@ window.onload = () => {
       const renderParques = async () => {
         var myHeaders = new Headers();
         const tblProdutos = document.getElementById("listagem");
-    
+        const popups = document.getElementById("popups");
         let strHtml = '';
-
+        let str2 = '';
         myHeaders.append("Content-Type", "application/json");
     
         var requestOptions = {
@@ -30,29 +30,151 @@ window.onload = () => {
         parques = parques.body;
         console.log("TESTANDO -----> "+parques.body)
         for (let i = 0; i < parques.length; i++) {
+            let idParque = parques[i].idParque;
             let nome = parques[i].nome; 
             let descricao = parques[i].descricao;
             let imagem = parques[i].imagem;
+            let capacidade = parques[i].capacidade;
+            let localizacao = parques[i].localizacao;
+            let latitude= parques[i].latitude;
+            let longitude = parques[i].longitude;
+            let mapa = parques[i].mapa;
     
           strHtml += `
           <img src="`+ `` + imagem + `" width="560" height="382" alt="">
                         <div class="hover_Text d-flex align-items-end justify-content-between">
                             <div class="hover_text_iner">
-                                <button type="place_btn" class="place_btn" onclick="openPopup()">Sobre</button>
+                                <button type="place_btn" class="place_btn" onclick="openPopup(${idParque})">Sobre</button>
                                 <h3>${nome}</h3>
                                 <br>
                                 <p>${descricao}</p>
                             </div>
                         </div>
-          
+                        
           `
+          str2 += `<div class="popup-screen" id="popup-${idParque}" >
+          <div class="popup-box" >
+              <span class="details"><h6> INFORMAÇÕES DO ${nome}</h6></span>
+              <br>
+              <span class="details"><h5> Localização: </h5></span><h5>${localizacao}</h5>
+              <span class="details"><h5> Lotação atual do parque:</h5></span><h5>${capacidade}</h5>
+              <span class="details"><h5> Coordenadas:</h5></span><h5>${latitude} , ${longitude}</h5>
+              <span class="details"><h5> Mapa do Parque:</h5></span><h5><img src="${mapa}" alt="image" class="profile-pic"></h5>
+              <a href="#" class="btn" onclick="closePopup(${idParque})">Ok</a>
+          </div>
+      </div>`
     
         }
         strHtml += "</tbody>"
         console.log("string hotml----------------" + strHtml)
         //loader.style.display="none"
         tblProdutos.innerHTML = strHtml
-    
+        popups.innerHTML = str2;
+
+        const header = document.getElementById("navbarSupportedContent");
+
+        if(getCookie("isLoggedIn")==""){
+
+          header.innerHTML = `<ul class="navbar-nav">
+          <li class="nav-item" class="active">
+              <a class="nav-link" href="home.html">Home</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="parques.html">Parques</a>
+          </li>
+  
+          <li class="nav-item">
+              <a class="nav-link" href="atividades.html">Atividades</a>
+          </li>
+          <!-- <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
+                  role="button" data-toggle="dropdown" aria-haspopup="true"
+                  aria-expanded="false">
+                  Atividades
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                  <a class="dropdown-item" href="riosmaisreq.html">Parque Jardim da Estrela</a>
+                  <a class="dropdown-item" href="riosmaiordif.html">Parque Eduardo VII</a>
+                  <a class="dropdown-item" href="riosmenordif.html">Parque Florestal de Monsanto</a>
+                  <a class="dropdown-item" href="riosmenordif.html">Parque da Bela Vista</a>
+              </div>
+          </li> -->
+          <li class="nav-item">
+              <a class="nav-link" href="forum.html">Fórum</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contacto</a>
+          </li>
+      </ul>
+      <a href="singin.html" class="btn_12">ENTRAR</a> `
+  
+      } else {
+          header.innerHTML =`<ul class="navbar-nav">
+          <li class="nav-item" class="active">
+              <a class="nav-link" href="home.html">Home</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="parques.html">Parques</a>
+          </li>
+  
+          <li class="nav-item">
+              <a class="nav-link" href="atividades.html">Atividades</a>
+          </li>
+          <!-- <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
+                  role="button" data-toggle="dropdown" aria-haspopup="true"
+                  aria-expanded="false">
+                  Atividades
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                  <a class="dropdown-item" href="riosmaisreq.html">Parque Jardim da Estrela</a>
+                  <a class="dropdown-item" href="riosmaiordif.html">Parque Eduardo VII</a>
+                  <a class="dropdown-item" href="riosmenordif.html">Parque Florestal de Monsanto</a>
+                  <a class="dropdown-item" href="riosmenordif.html">Parque da Bela Vista</a>
+              </div>
+          </li> -->
+          <li class="nav-item">
+              <a class="nav-link" href="forum.html">Fórum</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contacto</a>
+          </li>
+      </ul>
+      <a href="singin.html" class="btn_12" id="btnSair">Sair</a> `
+  
+          //logout();
+  
+      }
+      const botao = document.getElementById("btnSair");
+  
+  botao.addEventListener('click', async function (event) {
+  event.preventDefault();
+  logout();
+  window.location.href = './singin.html'
+  })
+  
+  
+  function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length,c.length);
+      }
+  }
+  return "";
+  }
+  
+  
+  function logout(){
+  delete_cookie("isLoggedIn")
+  }
+
+
         // Get the modal
         var modal = document.getElementById("modalP");
         /*   var modalD = document.getElementById("modald");  */
@@ -241,9 +363,29 @@ window.onload = () => {
         headers: myHeaders,
         credentials: 'include'
       };    
-    }
+
+      
+
+function setCookie(cname, cvalue, exdays) {
+const d = new Date();
+d.setTime(d.getTime() + (exdays*24*60*60*1000));
+let expires = "expires="+ d.toUTCString();
+document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+
+function delete_cookie(name) {
+document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+
+
     
-    function logout(){
+
+
+    
+    /*function logout(){
       const logout = document.getElementById("logout");
     
       logout.addEventListener("click", async function (event) {
@@ -272,8 +414,8 @@ window.onload = () => {
     
       });
       
-    };
+    };*/
     
     
     
-    
+  }
